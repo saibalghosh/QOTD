5a524f57b8111dcf40f66ec6f6a852051e36d6a7
Saibal Ghosh <Saibal Ghosh@FERMI>
2017-05-18 18:21:01 -0700

    Add project files.

---
+++ QOTD/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace QOTD
+{
+    [Activity(MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : BaseActivity
+    {
+
+        protected override int LayoutResource
+        {
+            get { return Resource.Layout.main; }
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SupportActionBar.SetDisplayHomeAsUpEnabled(false);
+            SupportActionBar.SetHomeButtonEnabled(false);
+        }
+    }
+}
+
